629cfd43476d1a96e0265e57ef5419c4407e8a49
LorhanPierre <lorhanpierre11102007@gmail.com>
2025-08-28 19:07:00 -0300

    fix: corrige o nome da variavel de entrada de dados

--- Atividade01.js
+++ Atividade01.js
@@ -1,6 +1,6 @@
-var prompt = require('prompt-sync')();
+var input = require('prompt-sync')();
 
-numero = parseInt(prompt("Digite um número para saber se ele é positivo ou negativo:"));
+numero = parseInt(input("Digite um número para saber se ele é positivo ou negativo:"));
 
 if(numero > 0){
     console.log("Número é positivo!");
