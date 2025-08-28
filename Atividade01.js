e3a430945a59c53a010fc7526c74cd55b5d8d4a4
LorhanPierre <lorhanpierre11102007@gmail.com>
2025-08-28 19:04:26 -0300

    cria código atividade01

---
+++ Atividade01.js
@@ -0,0 +1,11 @@
+var prompt = require('prompt-sync')();
+
+numero = parseInt(prompt("Digite um número para saber se ele é positivo ou negativo:"));
+
+if(numero > 0){
+    console.log("Número é positivo!");
+}else if(numero < 0){
+    console.log("Número é negativo!");
+}else{
+    console.log("O número é 0");
+};
