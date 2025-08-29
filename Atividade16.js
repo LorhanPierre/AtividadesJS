e7b3dedebdf2924eb9df4aa35965680aac5772a3
LorhanPierre <lorhanpierre11102007@gmail.com>
2025-08-29 14:38:27 -0300

    feat: cria codigo atividade02 parte 2

---
+++ Atividade16.js
@@ -0,0 +1,10 @@
+var input = require('prompt-sync')();
+
+valorA = parseInt(input("Digite o primeiro valor: "));
+valorB = parseInt(input("Digite o seguno valor para subtrair do primeiro: "));
+
+function subtrair(a,b){
+    return a-b;
+}
+
+console.log("Resultado é: "+subtrair(valorA,valorB));
