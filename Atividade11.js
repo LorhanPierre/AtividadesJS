e04ca8bbb897b6adf169e13e2ad04e7936ac31a5
LorhanPierre <lorhanpierre11102007@gmail.com>
2025-08-28 21:39:51 -0300

    feat: cria o código da atividade11

---
+++ Atividade11.js
@@ -0,0 +1,13 @@
+var numero  = 1;
+while(numero != 31){
+    if(numero % 3 == 0 && numero % 5 == 0){
+        console.log('FizzBuzz');
+    }else if(numero % 3 == 0){
+        console.log('Fizz');
+    }else if(numero % 5 == 0){
+        console.log('Buzz');
+    }else{
+        console.log(numero);
+    }
+    numero++;
+}
