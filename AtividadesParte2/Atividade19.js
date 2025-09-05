a2cb9e86fc42f3edc41e45c092c286e6cb13d5de
LorhanPierre <lorhanpierre11102007@gmail.com>
2025-09-05 20:41:26 -0300

    feat: Cria codigo atividade 5 da lista 2

--- AtividadesParte2/Atividade19.js
+++ AtividadesParte2/Atividade19.js
@@ -1,6 +1,43 @@
 var input = require('prompt-sync')();
 
-function calcularMedia(valores = []){
-
+function calcularMedia(values){
     
+    var resultadoSoma = 0
+
+    for(let i = 0; i <= values.length - 1; i++){
+        resultadoSoma += values[i];
+    }
+    console.log(resultadoSoma);
+
+    var media = resultadoSoma / values.length;
+
+    return media;
 }
+
+var finalizar = false;
+var valores = [];
+
+do{
+
+    console.log("----------------------------");
+    console.log("   calculadora de media")
+    console.log("----------------------------");
+    console.log(" 1 - Adicionar valor");
+    console.log(" 2 - Calcular");
+    console.log("----------------------------");
+    let escolha = parseInt(input(":"));
+
+    switch (escolha){
+        case 1:
+            let valor = parseFloat(input("valor: "));
+            valores.push(valor); 
+        break;
+        case 2: 
+        console.log(calcularMedia(valores))
+        finalizar = true; 
+        break;
+    }
+
+}while(!finalizar);
+
+
